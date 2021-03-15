055671ef78c4227602a0ef7d2670774c2cd6d5f4
gabriel123434534634 <fiuzagabri@gmail.com>
2021-03-15 13:29:45 -0300

    0.2.0

---
+++ server.py
@@ -0,0 +1,9 @@
+import os
+from bottle import route, run,template, static_file, get
+@route("/<filename:path>")
+def static(filename):
+    return static_file(filename, root=".")
+@route('/')
+def index():
+    return template("index.html", nome="Gabriel", static=static)
+run(host='0.0.0.0', port=os.environ.get("PORT", 5000))
